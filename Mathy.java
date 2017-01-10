f42ce4d4b1596f53666b5523dfce6c2cbdb3d541
hvkalidindi <HVKalidindi@student.naperville203.org>
2017-01-10 09:50:59 -0600

    Commit

---
+++ Mathy.java
@@ -0,0 +1,38 @@
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+//import javax.swing.*;
+public class Mathy 
+{
+    private int area = 0;
+    /**
+    default constructor - stubed in
+    */ 
+   public Mathy( ) 
+   {
+       System.out.println ("hi from default constructor" );
+       
+   }//end of default const
+    /**
+    take the absoulute value of the number @param int value @return absoulute value of number
+    *
+   public int abs (int x) 
+   {
+       //if (x<0)
+       
+    }//end of abs
+    */
+    /**
+    two int parameters and adds them @param 2 int values @return int sum of values
+    */
+    public int add (int x, int y)
+    {
+        int z = x + y;
+        return 0;
+        
+    }//end of add
+    public double add (double x,double y)
+    {
+    return 0;
+    
+    } 
+}
